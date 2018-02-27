990e439fbc99178cb07b556e6d3f4e1825f63744
Michael W <baddaemon87@gmail.com>
2018-02-27 19:09:10 +0100

    wiki: Validate device specific pages
    * Restructure generator script to use template files
      * Also change error behavior: Existing page won't generate an error
        anymore
    * Modify ruby validator:
      * Restructure existing code into functions
      * Add function to load templates (the ones from scripts), fill them
        properly and compare to the device specific files
        (build, info, install)
      * Of course also check if the files exist
      * Additionally use wiki-relative path for outputs to decrease line length
        on error lines
    
    Change-Id: Id5ccfdff1a01bd641475e7a95b1ca55341b3edcf

--- test/validate.rb
+++ test/validate.rb
@@ -13,21 +13,36 @@ def yaml_to_json(yaml)
    YAML.load_file(yaml).to_json
 end
 
-schema_path = File.expand_path('schema-06.yml', __dir__)
-schema = yaml_to_json(schema_path)
-
-sample_path = File.expand_path('../device_sample/sample.yml', __dir__)
-begin
-  sample = yaml_to_json(sample_path)
-rescue Psych::SyntaxError => e
-  puts 'Error parsing sample file: ' + e.message
+def to_relative_path(path)
+  wiki_dir = File.expand_path('../', __dir__) + '/'
+  return path.sub(wiki_dir, '')
 end
 
-begin
-  JSON::Validator.validate!(schema, sample, :validate_schema => true)
-rescue JSON::Schema::ValidationError => e
-  puts sample_path + ': ' + e.message
+def validate_json(schema, device_path)
+  device = yaml_to_json(device_path)
+  JSON::Validator.validate!(schema, device, :validate_schema => true)
+rescue JSON::Schema::ValidationError, Psych::SyntaxError => e
+  puts to_relative_path(device_path) + ': ' + e.message
   at_exit { exit false }
+end
+
+def load_template(template_file)
+  template_dir = File.expand_path('../scripts/templates', __dir__) + '/'
+  return File.open(template_dir + template_file, 'r') { |file| file.read }
+end
+
+def validate_template(template, path, codename)
+  if File.file?(path)
+    template_content = template.gsub('{codename}', codename)
+    file_content = File.open(path, 'r') { |file| file.read }
+    if not template_content == file_content
+      puts to_relative_path(path) + ': Not generated from template'
+      at_exit { exit false }
+    end
+  else
+    puts 'Missing file for ' + codename + ' at ' + path
+    at_exit { exit false }
+  end
 end
 
 trap "SIGINT" do
@@ -35,16 +50,34 @@ trap "SIGINT" do
   exit 130
 end
 
-device_dir = File.expand_path('../_data/devices/', __dir__) + '/'
+schema_path = File.expand_path('schema-06.yml', __dir__)
+schema = yaml_to_json(schema_path)
+
+sample_path = File.expand_path('../device_sample/sample.yml', __dir__)
+validate_json(schema, sample_path)
+
+wiki_dir = File.expand_path('../', __dir__) + '/'
+device_dir = wiki_dir + '_data/devices/'
+pages = wiki_dir + 'pages/'
+build_dir = pages + 'build/'
+info_dir = pages + 'info/'
+install_dir = pages + 'install/'
+
+# load once, these are equal across all devices
+build_template = load_template('build.md')
+info_template = load_template('info.md')
+install_template = load_template('install.md')
+
 Dir.entries(device_dir).each do |filename|
   device_path = device_dir + filename
   if File.file?(device_path)
-    begin
-      device = yaml_to_json(device_path)
-      JSON::Validator.validate!(schema, device, :validate_schema => true)
-    rescue JSON::Schema::ValidationError, Psych::SyntaxError => e
-      puts device_path + ': ' + e.message
-      at_exit { exit false }
-    end
+    validate_json(schema, device_path)
+
+    codename = filename.sub('.yml', '')
+    test_file = codename + '.md'
+
+    validate_template(build_template, build_dir + test_file, codename)
+    validate_template(info_template, info_dir + test_file, codename)
+    validate_template(install_template, install_dir + test_file, codename)
   end
 end
