52838a970c4d26c29d198ca5de8b51201ac1bf56
Michael W <baddaemon87@gmail.com>
2023-06-24 12:24:44 +0200

    wiki: prepare_image: Trim & Resize
    * Since many don't manage to trim the image properly, let the script do
      it
    * Enlarge pictures that are too small
    * Show a note when width==height==500 that this is an unlikely result
    
    Change-Id: Ic004aab770bb18ab197c25c2ae70db85969c4b89

--- scripts/prepare_image.sh
+++ scripts/prepare_image.sh
@@ -17,21 +17,26 @@ SMALL_IMAGE_DIR="$IMAGE_DIR/small"
 IMAGE_PATH="$IMAGE_DIR/$IMAGE"
 SMALL_IMAGE_PATH="$SMALL_IMAGE_DIR/$IMAGE"
 
-
 if [ ! -f $IMAGE_PATH ]; then
   echo "The specified image name doesn't exist!"
   exit 1
 fi
+
+# Trim the image
+mogrify -path $IMAGE_DIR -trim +repage $IMAGE_PATH
 
 RESULT=$(identify -format "%[fx:w]x%[fx:h]" $IMAGE_PATH)
 WIDTH=$(echo $RESULT | cut -d 'x' -f 1)
 HEIGHT=$(echo $RESULT | cut -d 'x' -f 2)
 
 if [ $WIDTH -gt 500 ] || [ $HEIGHT -gt 500 ]; then
-    echo "The original image is too large, resizing to max. 500x500"
+    echo "The trimmed image is too large, resizing to max. 500x500"
     mogrify -path $IMAGE_DIR -resize 500x500\> $IMAGE_PATH
-else
-    echo "Original image is $WIDTH x $HEIGHT -> OK"
+elif [ $WIDTH -lt 500 ] && [ $HEIGHT -lt 500 ]; then
+    echo "The trimmed image is smaller than the target size and will be enlarged. Please check the resulting image's quality!"
+    mogrify -path $IMAGE_DIR -resize 500x500\< $IMAGE_PATH
+elif [ $WIDTH -eq 500 ] && [ $HEIGHT -eq 500 ]; then
+    echo "The trimmed image has exactly equal width and height. This is most likely wrong, please check the wiki!"
 fi
 
 echo "Generating small image at $SMALL_IMAGE_PATH"
