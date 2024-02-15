c7766ee68a3677d72ed78893d5f297b7ff1a88ff
Nolen Johnson <johnsonnolen@gmail.com>
2024-02-15 00:03:33 -0500

    devices: Promote the Pixel 4 & XL
    * Face Unlock has a quirk now, and a Charter exemption.
    
    Change-Id: Ie5a5dc62b58b0afe348118b30a8d1008425ce70a

---
+++ pages/quirks/face_unlock.mk
@@ -0,0 +1,17 @@
+---
+sidebar: home_sidebar
+title: Quirks - Face Unlock
+permalink: quirks/face_unlock/
+robots: noindex
+search: exclude
+toc: false
+---
+
+## Face Unlock
+
+You have been redirected here because LineageOS builds for your device do not support face unlock biometric functions.
+
+### Why is that?
+
+Face unlock was poorly standardized, with this device's implementation being proprietary, and broken on newer Android versions.
+This might or might not affect you, depending on your usecase.
