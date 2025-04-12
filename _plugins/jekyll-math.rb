c3196fd19035c2a62f400fa9e1050c921f36455f
LuK1337 <priv.luk@gmail.com>
2025-04-12 10:22:34 +0200

    wiki: Merge math related plugins into jekyll-math
    Change-Id: I5db14a5c1aadcd6dc98c73a4777bb27bf4ac3777

---
+++ _plugins/jekyll-math.rb
@@ -0,0 +1,23 @@
+module Jekyll
+  class < Module
+    def in_to_mm(value)
+      (value * 25.4).round(2).to_s().chomp('.0')
+    end
+
+    def mm_to_in(value)
+      (value / 25.4).round(2).to_s().chomp('.0')
+    end
+
+    def ppi(input, screen_size)
+      h, w = input.split("x").map(&:to_i)
+      ("%d" % (Math.sqrt((h ** 2) + (w ** 2)) / screen_size))
+    end
+
+    # https://stackoverflow.com/a/60243022
+    def precision(input, value=0)
+      ("%.#{value}f" % input)
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::Math)
